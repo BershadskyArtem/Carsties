782a47c4f79a9cd9a0836ad1471a8ece949888af
Bershadsky Artem <bershadskyartem@gmail.com>
2023-12-24 00:19:40 +0300

    End of section 6. Add gateway service.

---
+++ AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using AuctionService.Data;
+using Contracts;
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _auctionDbContext;
+
+    public BidPlacedConsumer(AuctionDbContext auctionDbContext)
+    {
+        _auctionDbContext = auctionDbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        var message = context.Message;
+
+        if (Guid.TryParse(message.AuctionId, out var auctionId))
+        {
+            throw new ArgumentException(nameof(message.AuctionId));
+        }
+
+        var biddenAuction = await _auctionDbContext.Auctions.FirstOrDefaultAsync(a => a.Id == auctionId);
+
+        if (biddenAuction is null)
+            throw new NullReferenceException(nameof(biddenAuction));
+
+        //TODO: Remove hard coded string from contains method.
+        if (biddenAuction.CurrentHighBid is null 
+            || message.BidStatus.Contains("Accepted") 
+            && message.Amount > biddenAuction.CurrentHighBid)
+        {
+            biddenAuction.CurrentHighBid = message.Amount;
+
+            await _auctionDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,36 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var message = context.Message;
+        
+        var finishedAuction = await DB.Find<Item>().OneAsync(message.AuctionId);
+
+        if (finishedAuction is null)
+            throw new NullReferenceException(nameof(finishedAuction));
+
+        if (message.ItemSold)
+        {
+            if (message.Winner is null)
+                throw new NullReferenceException(nameof(message.Winner));
+
+            if (message.Amount is null)
+                throw new NullReferenceException(nameof(message.Amount));
+            
+            finishedAuction.Winner = message.Winner;
+            finishedAuction.SoldAmount = (int)message.Amount;
+        }
+
+        //TODO:Replace hardcoded string value.
+        finishedAuction.Status = "Finished";
+
+        await finishedAuction.SaveAsync();
+    }
+}
